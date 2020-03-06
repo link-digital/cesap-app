fd761ee637cbe394aa9f985bdae67dd503682c81
Andrés González Orjuela <iiandresii@gmail.com>
2020-03-06 15:17:08 -0500

    Archivo config

---
+++ app/config.js
@@ -0,0 +1,14 @@
+export default {
+  dev: {
+    ezappApi: 'http://www.cesap.gov.co/ezapp/',
+    otherReservationsApi: 'http://www.cesap.gov.co/api/v1/otras_reservas/'
+  },
+  stage: {
+    ezappApi: 'http://www.cesap.gov.co/ezapp/',
+    otherReservationsApi: 'http://www.cesap.gov.co/api/v1/otras_reservas/'
+  },
+  production: {
+    ezappApi: 'http://www.cesap.gov.co/ezapp/',
+    otherReservationsApi: 'http://www.cesap.gov.co/api/v1/otras_reservas/'
+  }
+}
